9844bb61936d5721ba7b21ca2785651904c5f787
그거 <chlwjddnr0116@gmail.com>
2025-07-05 05:10:35 +0000

    Revise : revise defualt url

--- armyvsproject-client/src/modules/global.js
+++ armyvsproject-client/src/modules/global.js
@@ -1,10 +1,10 @@
-const baseUrl = 'https://armyvsproject-server-hhvck.run.goorm.site/'
+const baseUrl = 'armyvsvsproject.com'
 
 const maxSelectionPerGame = 10
 
 const shareKakaoImage = 'https://ifh.cc/g/1vWa93.png'
 
-const clientUrl = 'https://armyvsproject-client-esyuo.run.goorm.site/'
+const clientUrl = 'armyvsproject.com'
 
 const reactAppKey = '1560e0542fa31994e719bf6ac5290ac4'
 
